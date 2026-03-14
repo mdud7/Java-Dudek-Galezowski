0071f4df7180c0c7c0b11ed67832329632296919
michal <mgalezowski@gmail.com>
2026-03-14 09:15:10 +0100

    dodano interfejs ProjectRepository

---
+++ ProjectManagerApp/src/main/java/org/example/projectmanagerapp/entity/ProjectRepository.java
@@ -0,0 +1,7 @@
+package org.example.projectmanagerapp.entity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+
+}
