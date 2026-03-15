dd1d412d5c591da3bc68ddc484194cb2f2ad9e6d
marcin <dudem1211@gmail.com>
2026-03-15 10:15:37 +0100

    drobne poprawki w plikach i rozszerzenu .java

---
+++ ProjectManagerApp/src/main/java/org/example/projectmanagerapp/controller/ProjectController.java
@@ -0,0 +1,7 @@
+package org.example.projectmanagerapp.controller;
+
+import org.example.projectmanagerapp.entity.Project;
+import org.example.projectmanagerapp.repository.ProjectRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
--- ProjectManagerApp/src/main/java/org/example/projectmanagerapp/repository/ProjectRepository.java
+++ ProjectManagerApp/src/main/java/org/example/projectmanagerapp/repository/ProjectRepository.java
@@ -1,7 +1,8 @@
-package org.example.projectmanagerapp.entity;
+package org.example.projectmanagerapp.repository;
 
+import org.example.projectmanagerapp.entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 public interface ProjectRepository extends JpaRepository<Project, Long> {
 
 }
